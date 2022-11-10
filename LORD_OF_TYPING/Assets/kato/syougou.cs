8fd16af38e819372f9b0c46d2f89f24e781a52e2
rio812 <ttc22c1020015@edu.tech.ac.jp>
2022-11-10 15:55:40 +0900

    add

---
+++ LORD_OF_TYPING/Assets/kato/syougou.cs
@@ -0,0 +1,223 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class syougou : MonoBehaviour
+{
+    void Start()
+    {
+       
+    }
+
+   
+
+void Update()
+    {
+        int point = 0;
+
+        if (point = 0 < 5000)
+        {
+            D;
+        }
+
+        if (point = 5000 < 10000)
+        {
+            C;
+        }
+
+        if (point = 10000 < 15000)
+        {
+            B;
+        }
+
+        if (point = 15000 < 20000)
+        {
+            A;
+        }
+
+        if (point = 20000 < 25000)
+        {
+            S;
+        }
+
+ //A‚ÌÌ†‚P`‚P‚R
+    public int A = 0;
+A = Random.Range(1, 14);
+switch (A)
+{
+    case 1:
+        return "—EŽÒ";
+
+    case 2:
+        return "’´‰zŽÒ";
+
+    case 3:
+        return "Œ«ŽÒ";
+
+    case 4:
+        return "Œ•¹";
+
+    case 5:
+        return "’²’âŽÒ";
+
+    case 6:
+        return "¹‹RŽm";
+
+    case 7:
+        return "‘Â“VŽg";
+
+    case 8:
+        return "”e‰¤";
+
+    case 9:
+        return "–‚pŽt";
+
+    case 10:
+        return "’é‰¤";
+
+    case 11:
+        return "•sŽ€’¹";
+
+    case 12:
+        return "‘–¯“IƒAƒCƒhƒ‹";
+
+    case 13:
+        return "”eŽÒ";
+}
+
+//B‚ÌÌ†‚P`‚P‚T
+public int B;
+B = Random.Range(1, 16);
+switch (B)
+{
+    case 1:
+        return "“±Žt";
+
+    case 2:
+        return "ŽçŒìŽÒ";
+
+    case 3:
+        return "‹“¹ŽÒ";
+
+    case 4:
+        return "Šó–]";
+
+    case 5:
+        return "â–]";
+
+    case 6:
+        return "˜B‹àpŽt";
+
+    case 7:
+        return "¸—ì";
+
+    case 8:
+        return "ˆ«–‚";
+
+    case 9:
+        return "IŽÒ";
+
+    case 10:
+        return "íŽm";
+
+    case 11:
+        return "’´l";
+
+    case 12:
+        return "‹­ŽÒ";
+
+    case 13:
+        return "Žh‹q";
+
+    case 14:
+        return "ˆê“™¯";
+
+    case 15:
+        return "‰»g";
+}
+
+//C‚ÌÌ†‚P`‚P‚O
+public int C;
+C = Random.Range(1, 11);
+switch (C)
+{
+    case 1:
+        return "’§íŽÒ";
+
+    case 2:
+        return "ŠwŽÒ";
+
+    case 3:
+        return "‹AŠÒŽÒ";
+
+    case 4:
+        return "b";
+
+    case 5:
+        return "‰¹F";
+
+    case 6:
+        return "’žŽ™";
+
+    case 7:
+        return "ŽÀ—ÍŽÒ";
+
+    case 8:
+        return "’T‹†ŽÒ";
+
+    case 9:
+        return "Œ¶‰e";
+
+    case 10:
+        return "ˆîÈ";
+}
+
+//D‚ÌÌ†‚P`‚W
+public int D;
+D = Random.Range(1, 9);
+switch (D)
+{
+    case 1:
+        return "–ÚŒ‚ŽÒ";
+
+    case 2:
+        return "V¯";
+
+    case 3:
+        return "•š•º";
+
+    case 4:
+        return "Øl";
+
+    case 5:
+        return "’T–KŽÒ";
+
+    case 6:
+        return "ŽáŽÒ";
+
+    case 7:
+        return "V•Ä";
+
+    case 8:
+        return "—ë";
+}
+
+//S‚ÌÌ†‚P`‚S
+public int S = 0;
+S = Random.Range(1, 5);
+switch (S)
+{
+    case 1:
+        return "‘ÅŒ®‹@_";
+
+    case 2:
+        return "‘ÅŒ®—´_";
+
+    case 3:
+        return "‘ÅŒ®Ž×_";
+
+    case 4:
+        return "‘ÅŒ®_";
+}
+   }
+}
