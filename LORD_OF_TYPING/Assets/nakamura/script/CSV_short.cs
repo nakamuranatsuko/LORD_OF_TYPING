cbe825b2a3db7bdecc65c3c88ecce18ca8508b35
nakamura natsuko <107166418+nakamuranatsuko@users.noreply.github.com>
2022-11-15 19:57:22 +0900

    1115_Add

---
+++ LORD_OF_TYPING/Assets/nakamura/script/CSV_short.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Text;
+
+public class CSV_short : MonoBehaviour
+{
+    public TextAsset csvFile; // CSVファイル
+    public static List<string[]> csvDatas = new List<string[]>(); // CSVの中身を入れるリスト
+
+    Encoding encoding;
+
+    void Start()
+    {
+        this.encoding = Encoding.GetEncoding("utf-8");
+        csvFile = Resources.Load("CSV_LORD_OF_TYPING_short_UTF-8") as TextAsset; // Resources下のCSV読み込み
+        StringReader reader = new StringReader(csvFile.text);
+
+        // , で分割しつつ一行ずつ読み込み
+        // リストに追加していく
+        while (reader.Peek() != -1) // reader.Peekが-1になるまで
+        {
+            string line = reader.ReadLine(); // 一行ずつ読み込み
+            csvDatas.Add(line.Split(',')); // , 区切りでリストに追加
+        }
+    }
+}
--- LORD_OF_TYPING/Assets/nakamura/script/gaugeManager.cs
+++ LORD_OF_TYPING/Assets/nakamura/script/gaugeManager.cs
@@ -6,9 +6,17 @@ using UnityEngine.UI;
 public class gaugeManager : MonoBehaviour
 {
     public Image gauge;//ゲージ画像
-    public static int gaugeTime = 30;//ゲージ管理用
+    public int gaugeTime = 30;//ゲージ管理用
+    public int input_success = 15;//タイピング成功時のゲージ増加数
+    public static int gauge_flg = 0;//ゲージがすべて埋まった時用
+    public float num = 0;
 
     public float time;
+
+    void Start()
+    {
+        gauge_flg = 0;
+    }
 
     void Update()
     {
@@ -16,9 +24,11 @@ public class gaugeManager : MonoBehaviour
         if(time <= 0.0)
         {
             time = 1.0f;
-            gaugeManager.gaugeTime -= 1;//ゲージが1減る
+            gaugeTime -= 1;//ゲージが1減る
         }
-        if (inputField_Comp.flg == 1) gaugeManager.gaugeTime += 5;//ゲージが5増える
-        gauge.fillAmount = gaugeTime / 100.0f;//ゲージに反映させる
+        if(inputField_Comp.flg == 1) gaugeTime += input_success;//ゲージを増やす
+        num = gaugeTime / 100.0f;
+        gauge.fillAmount = num;//ゲージに反映させる
+        if(gauge.fillAmount >= 1) gauge_flg = 1;//ゲージが1になったらフラグを立たせる
     }
 }
